2c9d055b38a2552f4c9e576816fdb8dfb7de7ba3
Stephanie Hill <stephni.hill@gmail.com>
2023-03-14 20:29:47 -0700

    chart

--- class-12/Chart-Demo/app.js
+++ class-12/Chart-Demo/app.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Chartreuse', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+        backgroundColor: [
+          'red',    // color for data at index 0
+          'blue',   // color for data at index 1
+          'green',  // color for data at index 2
+          'black',  // color for data at index 3
+          //...
+      ]
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
